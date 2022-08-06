012e2151977c1eeef73e3681bc4fb5590334b6db
kkj53051000 <tkarnrwl7418@naver.com>
2022-08-06 10:17:47 +0900

    b2609 success

---
+++ baekjoon_math/b2609.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/2609
+# 최대공약수 최소공배수
+
+
+from tkinter import Y
+
+
+x, y = map(int, input().split())
+
+
+# 최대 공약수
+def GCD(x, y):
+    while(True):
+        r = x % y
+
+        if r == 0:
+            break
+
+        x = y
+        y = r
+    
+    return y
+
+# 최소 공배수
+def LCM(x, y):
+    result = (x*y)//GCD(x,y)
+    return result
+    
+
+print(GCD(x, y))
+print(LCM(x, y))
