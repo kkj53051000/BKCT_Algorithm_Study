d340a8ad2b7c7ef9451721d3e77f2a0dc6612252
kkj53051000 <tkarnrwl7418@naver.com>
2022-08-06 16:30:52 +0900

    b4948 success

--- baekjoon_math/b4948.py
+++ baekjoon_math/b4948.py
@@ -1,26 +1,31 @@
 # https://www.acmicpc.net/problem/4948
-
+# 에라토스테네스의 체
 import math
 import sys
 
+maxValue = 123456
+
+arr = [True for i in range(0, (maxValue * 2) + 1)]
+
+for i in range(2, int(math.sqrt(maxValue * 2) + 1)):
+    
+    if arr[i]:
+        for j in range(i*i, len(arr)):
+            if j % i == 0:
+                arr[j] = False
 
 while True:
     
     n = int(sys.stdin.readline())
-    sum = n
 
     if n == 0:
         break
 
-    arr = [True for i in range(0, (n*2)+1)]
+    
+    answer = 0
 
-
-    for i in range(2, int(math.sqrt(n*2) + 1)):
-        
+    for i in range(n+1, (n*2)+1):
         if arr[i]:
-            for j in range(n+1, len(arr)):
-
-                if arr[j] and j != i and j % i == 0:
-                    arr[j] = False
-                    sum -= 1
-    print(sum)
+            answer +=1
+    
+    print(answer)
