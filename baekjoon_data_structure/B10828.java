7b6a6df2441070e5e8ee41dfd84a587d35af21e4
kkj53051000 <tkarnrwl7418@naver.com>
2022-08-28 14:52:42 +0900

    fix B10828

--- baekjoon_data_structure/B10828.java
+++ baekjoon_data_structure/B10828.java
@@ -6,67 +6,75 @@ import java.util.List;
 
 public class B10828 {
 
-    private static List<Integer> arr = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int size = Integer.parseInt(reader.readLine());
 
+        InputAndPrint inputAndPrint = new InputAndPrint();
+
         for (int i = 0; i < size; i++) {
             String input = reader.readLine();
 
-            if (input.equals("pop")) {
-                pop();
-            }else if (input.equals("size")) {
-                size();
-            }else if (input.equals("empty")) {
-                empty();
-            }else if (input.equals("top")) {
-                top();
-            }else { // push
-                push(input);
-            }
-
+            inputAndPrint.decodeAndExecute(input);
         }
     }
 
-    public static void pop() {
-        if (arr.size() == 0) {
+
+}
+
+class InputAndPrint {
+    private List<Integer> stack = new ArrayList<>();
+
+    public void decodeAndExecute(String input) {
+        if (input.equals("pop")) {
+            pop();
+        }else if (input.equals("size")) {
+            size();
+        }else if (input.equals("empty")) {
+            empty();
+        }else if (input.equals("top")) {
+            top();
+        }else { // push
+            push(input);
+        }
+    }
+
+    public void pop() {
+        if (stack.size() == 0) {
             System.out.println(-1);
         }else {
-            int lastIndex = arr.size()-1;
-            System.out.println(arr.get(lastIndex));
-            arr.remove(lastIndex);
+            int lastIndex = stack.size()-1;
+            System.out.println(stack.get(lastIndex));
+            stack.remove(lastIndex);
         }
     }
 
-    public static void size() {
-        System.out.println(arr.size());
+    public void size() {
+        System.out.println(stack.size());
     }
 
-    public static void empty() {
-        if (arr.size() == 0) {
+    public void empty() {
+        if (stack.size() == 0) {
             System.out.println(1);
         }else {
             System.out.println(0);
         }
     }
 
-    public static void top() {
-        if (arr.size() == 0) {
+    public void top() {
+        if (stack.size() == 0) {
             System.out.println(-1);
         }else {
-            int lastIndex = arr.size()-1;
-            System.out.println(arr.get(lastIndex));
+            int lastIndex = stack.size()-1;
+            System.out.println(stack.get(lastIndex));
         }
     }
 
-    public static void push(String input) {
+    public void push(String input) {
         String[] strArr = input.split(" ");
         int value = Integer.parseInt(strArr[1]);
 
-        arr.add(value);
+        stack.add(value);
     }
-
 }
