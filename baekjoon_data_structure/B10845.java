74467afbe4a21b1858b86127351b2e447ced673b
kkj53051000 <tkarnrwl7418@naver.com>
2022-08-28 14:58:25 +0900

    fix B10845

--- baekjoon_data_structure/B10845.java
+++ baekjoon_data_structure/B10845.java
@@ -6,76 +6,87 @@ import java.util.List;
 
 public class B10845 {
 
-    private static List<Integer> arr = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int size = Integer.parseInt(reader.readLine());
 
+        InputAndPrint2 inputAndPrint = new InputAndPrint2();
+
         for (int i = 0; i < size; i++) {
             String input = reader.readLine();
 
-            if (input.equals("pop")) {
-                pop();
-            }else if (input.equals("size")) {
-                size();
-            }else if (input.equals("empty")) {
-                empty();
-            }else if (input.equals("front")) {
-                front();
-            }else if (input.equals("back")) {
-                back();
-            }else { // push
-                push(input);
-            }
-
+            inputAndPrint.decodeAndExecute(input);
         }
     }
 
-    public static void pop() {
-        if (arr.size() == 0) {
+
+
+}
+
+
+class InputAndPrint2 {
+    private List<Integer> queue = new ArrayList<>();
+
+    public void decodeAndExecute(String input) {
+        if (input.equals("pop")) {
+            pop();
+        } else if (input.equals("size")) {
+            size();
+        } else if (input.equals("empty")) {
+            empty();
+        } else if (input.equals("front")) {
+            front();
+        } else if (input.equals("back")) {
+            back();
+        } else { // push
+            push(input);
+        }
+    }
+
+    public void pop() {
+        if (queue.size() == 0) {
             System.out.println(-1);
-        }else {
-            System.out.println(arr.get(0));
-            arr.remove(0);
+        } else {
+            System.out.println(queue.get(0));
+            queue.remove(0);
         }
     }
 
-    public static void size() {
-        System.out.println(arr.size());
+    public void size() {
+        System.out.println(queue.size());
     }
 
-    public static void empty() {
-        if (arr.size() == 0) {
+    public void empty() {
+        if (queue.size() == 0) {
             System.out.println(1);
-        }else {
+        } else {
             System.out.println(0);
         }
     }
 
-    public static void front() {
-        if (arr.size() == 0) {
+    public void front() {
+        if (queue.size() == 0) {
             System.out.println(-1);
-        }else {
-            System.out.println(arr.get(0));
+        } else {
+            System.out.println(queue.get(0));
         }
     }
 
-    public static void back() {
-        if (arr.size() == 0) {
+    public void back() {
+        if (queue.size() == 0) {
             System.out.println(-1);
-        }else {
-            int lastIndex = arr.size()-1;
-            System.out.println(arr.get(lastIndex));
+        } else {
+            int lastIndex = queue.size() - 1;
+            System.out.println(queue.get(lastIndex));
         }
     }
 
-    public static void push(String input) {
+    public void push(String input) {
         String[] strArr = input.split(" ");
         int value = Integer.parseInt(strArr[1]);
 
-        arr.add(value);
+        queue.add(value);
     }
-
 }
+
